3c2b64bab248c48cb3c95045d21608097273a370
Himalee <himalee.tailor@gmail.com>
2018-09-26 11:31:40 +0100

    Update: test for status code 200

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -11,9 +11,15 @@ describe 'The TicTacToe App' do
     Sinatra::Application
   end
 
-  it "says Welcome to Tic Tac Toe" do
-    get '/game'
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('Welcome to Tic Tac Toe')
+  context "/game" do
+    it "returns status 200 OK" do
+      response = get '/game'
+      expect(response.status).to eq 200
+    end
+
+    it "says Welcome to Tic Tac Toe" do
+      get '/game'
+      expect(last_response.body).to eq('Welcome to Tic Tac Toe')
+    end
   end
 end
