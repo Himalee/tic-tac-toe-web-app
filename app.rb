e33b152c522d6ccc08998cc5a00fcdd0a7946c5d
Himalee <himalee.tailor@gmail.com>
2018-10-02 15:58:17 +0100

    Test & add: switch marks when clicking buttons on grid

--- app.rb
+++ app.rb
@@ -1,16 +1,39 @@
 require 'sinatra'
 require 'erb'
+require 'json'
 
 get '/game' do
   @grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  store_grid(@grid)
   erb :game, { :layout => true }
 end
 
 post '/game/play' do
-  @grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-  guess = params["cell"]
-  @grid[guess.to_i] = "X"
+  @grid = get_grid
+  move = params["cell"]
+  if @grid.count("X") == 0 && @grid.count("O") == 0
+    mark = "X"
+  elsif @grid.count("X") == @grid.count("O")
+    mark = "X"
+  else
+    mark = "O"
+  end
+  @grid[move.to_i] = mark
+  store_grid(@grid)
   erb :game, { :layout => true }
+end
+
+def store_grid(grid)
+  hash = { "grid" => grid }
+  File.open("public/grid.json", "w") do |file|
+    file.write(hash.to_json)
+  end
+end
+
+def get_grid
+  file = File.read("public/grid.json")
+  data_hash = JSON.parse(file)
+  data_hash["grid"]
 end
 
 
