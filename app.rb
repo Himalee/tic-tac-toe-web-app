94fb623f3ed053557bbffd75c9f276d7defba983
Himalee <himalee.tailor@gmail.com>
2018-10-10 14:31:02 +0100

    Refactor: add before block to runner file

--- app.rb
+++ app.rb
@@ -3,28 +3,29 @@ require 'erb'
 require 'json'
 require_relative 'lib/board.rb'
 
+before do
+  @board = Board.new
+end
+
 get '/game' do
-  board = Board.new
   @grid = Array.new(9, "")
-  board.store_grid(@grid)
+  @board.store_grid(@grid)
   erb :game, { :layout => true }
 end
 
 post '/game/play' do
-  board = Board.new
-  @grid = board.get_grid
+  @grid = @board.get_grid
   move = params["cell"]
-  board.store_marked_grid(@grid, move.to_i, board.get_mark(@grid))
-  if board.end_of_game?(@grid, "X", "O")
+  @board.store_marked_grid(@grid, move.to_i, @board.get_mark(@grid))
+  if @board.end_of_game?(@grid, "X", "O")
     redirect "/result"
   end
   erb :game, { :layout => true }
 end
 
 get "/result" do
-  board = Board.new
-  @grid = board.get_grid
-  @result = board.result(@grid)
+  @grid = @board.get_grid
+  @result = @board.result(@grid)
   erb :result, { :layout => true }
 end
 
