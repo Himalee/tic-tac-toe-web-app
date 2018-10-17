933135754630244bc1670b2d415bfca52aba6804
Himalee <himalee.tailor@gmail.com>
2018-10-17 16:21:19 +0100

    Test: computer player chooses moves to win

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -39,41 +39,54 @@ describe 'The TicTacToe App' do
     expect(page).to have_button("Human vs Computer")
   end
 
-  it "returns initial grid buttons" do
-    visit "/"
-    click_button "Human vs Human"
-    grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    grid.each do |cell_name|
-      expect(page).to have_button(cell_name)
+  context "given a human vs human game mode" do
+    it "returns initial grid buttons" do
+      visit "/"
+      click_button "Human vs Human"
+      grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+      grid.each do |cell_name|
+        expect(page).to have_button(cell_name)
+      end
+    end
+
+    it "replaces button value with mark" do
+      visit "/"
+      click_button "Human vs Human"
+      click_button "1"
+      expect(page).to have_button("X")
+    end
+
+    it "switches marks when clicking buttons" do
+      visit "/"
+      click_button "Human vs Human"
+      click_button "1"
+      click_button "2"
+      mark = ["X", "O"]
+      mark.each do |mark|
+        expect(page).to have_button(mark)
+      end
+    end
+
+    it "routes to /result when first player 'X' wins" do
+      visit "/"
+      click_button "Human vs Human"
+      click_button "0"
+      click_button "1"
+      click_button "3"
+      click_button "2"
+      click_button "6"
+      expect(page).to have_content("Game over")
     end
   end
 
-  it "replaces button value with mark" do
-    visit "/"
-    click_button "Human vs Human"
-    click_button "1"
-    expect(page).to have_button("X")
-  end
-
-  it "switches marks when clicking buttons" do
-    visit "/"
-    click_button "Human vs Human"
-    click_button "1"
-    click_button "2"
-    mark = ["X", "O"]
-    mark.each do |mark|
-      expect(page).to have_button(mark)
+  context "given a human vs computer game mode" do
+    it "returns 'game over'" do
+      visit "/"
+      click_button "Human vs Computer"
+      click_button "0"
+      click_button "1"
+      click_button "5"
+      expect(page).to have_content("Game over")
     end
-  end
-
-  it "routes to /result when first player 'X' wins" do
-    visit "/"
-    click_button "Human vs Human"
-    click_button "0"
-    click_button "1"
-    click_button "3"
-    click_button "2"
-    click_button "6"
-    expect(page).to have_content("Game over")
   end
 end
