64223ce88a3570077a0541d3f7cf42627dc96960
Himalee <himalee.tailor@gmail.com>
2018-10-18 16:16:16 +0100

    Refactor: use constants for game modes

--- models/game_factory.rb
+++ models/game_factory.rb
@@ -1,10 +1,13 @@
 class GameFactory
 
+HumanvsHuman = "Human vs Human"
+HumanvsComputer = "Human vs Computer"
+
   def game(game_mode)
-    if game_mode == "Human vs Human"
+    if game_mode == HumanvsHuman
       players = [HumanPlayer.new("X"), HumanPlayer.new("O")]
       Game.new(new_board, players)
-    elsif game_mode == "Human vs Computer"
+    elsif game_mode == HumanvsComputer
       players = [HumanPlayer.new("X"), ComputerPlayer.new("O")]
       Game.new(new_board, players)
     end
